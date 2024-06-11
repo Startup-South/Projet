937c0acb62c1ba5a2686fd0a68d975037d427605
Startup-South <mirodenormand@gmail.com>
2024-06-11 15:06:14 +0300

    Api product

--- Backend/app/Models/Product.php
+++ Backend/app/Models/Product.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'productname',
+        'image',
+        'price',
+        'description',
+        'quantity',
+        'is_available',
+    ];
 }
